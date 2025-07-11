8b6686a52d7ca32ceb92bdb84837bfd54d604c7d
DEV-JVP <josevp123lol@gmail.com>
2025-07-11 12:13:05 -0500

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite('resources/css/app.css')
+      <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="h-screen overflow-hidden flex items-center justify-center bg-gray-100" >
