4dbe54bcdf5c18d1713f7618e0822a96c39c922b
DEV-JVP <josevp123lol@gmail.com>
2025-07-11 12:12:36 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>Laravel</title>
 
+           <script src="https://cdn.tailwindcss.com"></script>
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
